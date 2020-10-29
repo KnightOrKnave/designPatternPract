5703c664095b88ae8c0f3a8ef0f1de8dd152a321
localuser <a11dc094@dhw.ac.jp>
2020-10-29 21:55:41 +0900

    singleton + 練習問題修正前

---
+++ Singleton/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Singleton
+{
+
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを作成しました");
+        }
+
+        public static Singleton getInstance()
+        {
+            return singleton;
+        }
+    }
+
+    public class TicketMaker
+    {
+        private int ticket = 1000;
+        public int getNextTicketNumber()
+        {
+            return ticket++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj1 = Singleton.getInstance();
+            Singleton obj2 = Singleton.getInstance();
+
+            if(obj1==obj2)
+            {
+                Console.WriteLine("same");
+            }
+            else
+            {
+                Console.WriteLine("different");
+            }
+
+
+            TicketMaker t1 = new TicketMaker();
+            TicketMaker t2 = new TicketMaker();
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(t1.getNextTicketNumber());
+                Console.WriteLine(t2.getNextTicketNumber());
+            }
+
+        }
+    }
+}
