e2da0a0fa078c3bd2b0cee2be6e093330b0f3421
localuser <a11dc094@dhw.ac.jp>
2020-11-01 17:32:07 +0900

    prototype実装

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -26,19 +26,83 @@ namespace Prototype
 
     public class MessageBox : Product
     {
+        private char dotchar;
+        public MessageBox(char dc)
+        {
+            this.dotchar = dc;
+        }
+
         public object Clone()
         {
-            throw new NotImplementedException();
+            return this;
         }
 
         public Product createClone()
         {
-            throw new NotImplementedException();
+            Product p = null;
+            try
+            {
+                p = (Product)Clone();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return p;
         }
 
         public void use(string s)
         {
-            throw new NotImplementedException();
+            for(int i=0;i<s.Length+4;i++)
+            {
+                Console.Write(this.dotchar);
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"{this.dotchar} {s} {this.dotchar}");
+            for (int i = 0; i < s.Length + 4; i++)
+            {
+                Console.Write(this.dotchar);
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public class UnderlinePen : Product
+    {
+        private char ulchar;
+        public UnderlinePen(char dc)
+        {
+            this.ulchar = dc;
+        }
+
+        public object Clone()
+        {
+            return this;
+        }
+
+        public Product createClone()
+        {
+            Product p = null;
+            try
+            {
+                p = (Product)Clone();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return p;
+        }
+
+        public void use(string s)
+        {
+            Console.WriteLine($@"""{s}""");
+            Console.Write(" ");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(this.ulchar);
+            }
+            Console.WriteLine(" ");
         }
     }
 
@@ -47,7 +111,22 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Manager manager = new Manager();
+            UnderlinePen upen = new UnderlinePen('~');
+            MessageBox mbox = new MessageBox('*');
+            MessageBox sbox = new MessageBox('/');
+            manager.register("strong message", upen);
+            manager.register("warning box", mbox);
+            manager.register("slash box", sbox);
+
+            Product p1 = manager.create("strong message");
+            Product p2 = manager.create("warning box");
+            Product p3 = manager.create("slash box");
+
+            p1.use("Hello,World. ");
+            p2.use("Hello,World. ");
+            p3.use("Hello,World. ");
+
         }
     }
 }
