391dc9c77fae6235334b11dd0102ef800b732eab
localuser <a11dc094@dhw.ac.jp>
2020-11-16 08:51:32 +0900

    chain 実装済

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace ChainOfResponsibility
 {
@@ -18,7 +20,7 @@ namespace ChainOfResponsibility
 
         public override string ToString()
         {
-            return $"[Trouble {number}";
+            return $"[Trouble {number}]";
         }
     }
 
@@ -53,6 +55,10 @@ namespace ChainOfResponsibility
                 fail(trouble);
             }
         }
+        public override string ToString()
+        {
+            return $"[{name}]";
+        }
 
         protected abstract bool resolve(Trouble trouble);
         protected void done(Trouble trouble)
@@ -65,11 +71,119 @@ namespace ChainOfResponsibility
         }
     }
 
+    public class NoSupport : Support
+    {
+        public NoSupport(string name):base(name)
+        {
+        }
+
+        protected override bool resolve(Trouble trouble)
+        {
+            return false;
+        }
+    }
+
+    public class LimitSupport : Support
+    {
+        private int limit;
+        public LimitSupport(string name,int limit):base(name)
+        {
+            this.limit = limit;
+        }
+
+        protected override bool resolve(Trouble trouble)
+        {
+            if(trouble.getNumber()<limit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class OddSupport : Support
+    {
+
+        public OddSupport(string name) : base(name)
+        {
+
+        }
+
+        protected override bool resolve(Trouble trouble)
+        {
+            if(trouble.getNumber()%2==1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class EvenSupport : Support
+    {
+
+        public EvenSupport(string name) : base(name)
+        {
+
+        }
+
+        protected override bool resolve(Trouble trouble)
+        {
+            if (trouble.getNumber() % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class SpecialSupport : Support
+    {
+        private int number;
+        public SpecialSupport(string name,int number) : base(name)
+        {
+            this.number = number;
+        }
+
+        protected override bool resolve(Trouble trouble)
+        {
+            if (trouble.getNumber() == this.number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Support alice = new NoSupport("Alice");
+            Support bob = new LimitSupport("Bob", 100);
+            Support charlie = new SpecialSupport("Charlie", 429);
+            Support diana = new LimitSupport("Diana", 200);
+            Support elmo = new OddSupport("Elmo");
+            Support fred = new LimitSupport("Fred", 300);
+            alice.setNext(bob).setNext(charlie).setNext(diana).setNext(elmo).setNext(elmo).setNext(fred);
+
+            
+            for(int i=0;i<500;i+=33)
+            {
+                alice.support(new Trouble(i));
+            }
         }
     }
 }
