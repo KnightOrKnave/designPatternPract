27b88121e9d34ee85b86fda8ec24e6fd6d5a3e15
localuser <a11dc094@dhw.ac.jp>
2020-11-15 21:56:45 +0900

    add chain

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
