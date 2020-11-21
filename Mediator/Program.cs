43d5aec2d4ece18e150021e30290f6d4e72b7d56
localuser <a11dc094@dhw.ac.jp>
2020-11-21 19:55:00 +0900

    mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("コンソールアプリじゃないので保留");
+        }
+    }
+}
